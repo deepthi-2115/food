bbd3e01efa060b69466b82b45d67a5723324a1c1
Kottala deepthi <reddydeepuh@gmail.com>
2026-06-20 16:05:44 +0530

    Update Courses.jsx

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -14,7 +14,7 @@ const [sortOrder, setSortOrder] = useState("asc");
     expiryDate: "",
   });
 
-  const API_URL = import meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const fetchFoods = async () => {
     try {
